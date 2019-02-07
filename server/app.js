b03de779e724ff5d5698c338b47e6d02b9626221
Guannian Zeng <gzeng@bn.com>
2019-02-07 16:48:54 -0500

    graphql setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 app.listen(4000, () => {
 	console.log('app is listening on port 4000');
