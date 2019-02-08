704fc528958d59626f27ceb7deb1a171881ae7ed
Guannian Zeng <vgnzeng@gmail.com>
2019-02-08 09:27:32 -0500

    define rootQuery for 1 endpoint which takes BookType

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,34 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
+  field: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    genre: {type: graphQLString}
+  })
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  field: {
+    book: {
+      type: BookType,
+      args: {
+        id: { 
+          type: GraphQLString
+        }
+      },
+      resolve(parent, args) {
+          // code to get data from db/other source
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
+
