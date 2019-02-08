90a04ef0d1ae1409df3be0871f087c70c3669324
Guannian Zeng <vgnzeng@gmail.com>
2019-02-08 14:01:04 -0500

    update resolve functions for reading data from mongodb

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -39,6 +39,7 @@ const BookType = new GraphQLObjectType({
       resolve(parent, args) {
         // console.log(parent);
         // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       }
     }
   })
@@ -54,6 +55,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return _.filter(books, { authorId: parent.id })
+        return Book.find({ authorId: parent.id });
       }
     }
   })
@@ -68,6 +70,7 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db/other source
         // return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       }
     },
     author: {
@@ -75,18 +78,21 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       }
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return books;
+        return Book.find({});
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
         // return authors;
+        return Author.find({});
       }
     }
   }
