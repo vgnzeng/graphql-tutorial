8ce4ccf84e2f360ee30ad9996a49837f1c4939ec
Guannian Zeng <vgnzeng@gmail.com>
2019-02-08 12:18:12 -0500

    connect to local mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to database
+mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connection.once('open', () => {
+	console.log('connected to local mongodb');
+});
 
 app.use('/graphql', graphqlHTTP({
 	schema: schema,
