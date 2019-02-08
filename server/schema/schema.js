cb78e1b5314071a33f1e3cc9660fbc4a31d22962
Guannian Zeng <vgnzeng@gmail.com>
2019-02-08 11:16:03 -0500

    GraphQLList that returns list of types

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,8 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 //dummy data for now
@@ -14,6 +15,9 @@ var books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
   { name: 'Final Fantacy XXI', genre: 'Fantasy', id: '2', authorId: '2' },
   { name: 'The Long earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Mummy', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'Ironman 1', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'Ironman 2', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 var authors = [
@@ -43,7 +47,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id })
+      }
+    }
   })
 });
 
