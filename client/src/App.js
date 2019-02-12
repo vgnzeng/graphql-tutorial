abcd1c4c73175f4dd10829bd9ce888a314ed48cc
Guannian Zeng <vgnzeng@gmail.com>
2019-02-12 13:35:28 -0500

    add react component for book list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div className="main">
-        
+        <h1>Guan's Reading List</h1>
+        <BookList />
       </div>
     );
   }
