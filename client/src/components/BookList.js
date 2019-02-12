1ee27501118aad3ec868ae3815643e84a2917b78
Guannian Zeng <vgnzeng@gmail.com>
2019-02-12 15:58:41 -0500

    compose mutltiple queries into one component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -19,7 +19,7 @@ class BookList extends Component {
         }
     }
     render() {
-        // console.log(this.props);
+        console.log(this.props);
         return (
             <div>
                 <ul id="book-list">
